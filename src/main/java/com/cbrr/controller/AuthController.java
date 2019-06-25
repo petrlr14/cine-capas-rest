13c26475ae55d2751d7d22cea6afe56e05d8e54e
petrlr14 <pedrodeveloper14@gmail.com>
2019-06-24 23:42:48 -0600

    trying fix cors

--- src/main/java/com/cbrr/controller/AuthController.java
+++ src/main/java/com/cbrr/controller/AuthController.java
@@ -7,14 +7,12 @@ import com.cbrr.responses.auth.LogoutResponse;
 import com.cbrr.service.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/auth")
 public class AuthController {
 
